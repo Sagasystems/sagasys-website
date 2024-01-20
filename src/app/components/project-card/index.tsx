c6f6c69c849d153a4b7b6b821baa9698b64eb115
hash-luk <lucas.patrick2506@gmail.com>
2024-01-19 21:58:58 -0300

    feat: Added projects and configure new tw config

--- src/app/components/project-card/index.tsx
+++ src/app/components/project-card/index.tsx
@@ -14,8 +14,8 @@ export default function ProjectCard({
   dataWork,
 }: projectCardProps) {
   return (
-    <div className="rounded-lg w-full flex flex-row md:flex-col md:w-[324px] md:h-[400px] shadow-md hover:scale-105 transition-all ease-in-out duration-100 overflow-hidden bg-gray-100">
-      <div className="relative md:w-full w-[400px] max-w-[150%] md:h-[170px] h-auto">
+    <div className="rounded-lg w-full flex flex-row md:basis-1/3 basis-full md:flex-col md:w-[324px] md:h-[400px] shadow-md hover:scale-105 transition-all ease-in-out duration-100 overflow-hidden bg-gray-100">
+      <div className="relative md:w-full w-[400px] max-w-[90px] md:max-w-full md:h-[170px] h-auto">
         <Image
           src={imageSrc}
           alt={"Imagem do projeto " + project}
@@ -26,7 +26,9 @@ export default function ProjectCard({
       </div>
       <div className="md:w-full md:w-max-[276px] w-max-[198px] p-4">
         <p className="text-lg font-bold mb-4">{project}</p>
-        <p className="text-sm mb-8 text-gray-700">{description}</p>
+        <p className="text-sm mb-8 text-gray-700 min-h-[100px]">
+          {description}
+        </p>
         <p className="text-xs  text-gray-500">{dataWork}</p>
       </div>
     </div>
--- src/app/components/projects/index.tsx
+++ src/app/components/projects/index.tsx
@@ -10,12 +10,36 @@ export default function Projects() {
         <h3 className="m-0 text-3xl font-bold">Portfólio</h3>
         <div className="bg-orange-500 md:max-w-[45%] h-1 w-full"></div>
       </div>
-      <div className="flex flex-wrap w-full gap-8 justify-between">
+      <div className="flex flex-wrap w-full gap-8">
         <ProjectCard
           imageSrc="/assets/images/projects/marianafestas.jpg"
           project="Mariana Festas"
           description="Criação de uma landing page para uma empresa de promoção de eventos infantis. Prospecção de clientes e redirecionamento do cliente para o whatsapp. Fotos e cadastramento de clientes."
           dataWork="Maio 2023"
+        />
+        <ProjectCard
+          imageSrc="/assets/images/projects/tomdehumor.png"
+          project="Tom de Humor"
+          description="Criação de um blog para divulgação de eventos, shows e muito mais."
+          dataWork="Julho 2023"
+        />
+        <ProjectCard
+          imageSrc="/assets/images/projects/taskrush.png"
+          project="TaskRush"
+          description="Software para controle de demandas, equipes e cálculo de HH dos funcionários, de acordo com as regras de clientes e projetos."
+          dataWork="Novembro 2023"
+        />
+        <ProjectCard
+          imageSrc="/assets/images/projects/holder.png"
+          project="Holder+"
+          description="Criação de software para acompanhar seus investimentos globais e ativos patrimoniais em um único lugar, com o auxílio de várias ferramentas inéditas."
+          dataWork="Outubro 2023"
+        />
+        <ProjectCard
+          imageSrc="/assets/images/projects/atenderdigital.png"
+          project="AtenderDigital"
+          description="Criação de software para centralizar os atendimentos e melhorar a experiência do cliente com uma única plataforma! Integrações com telêfonia, Whatsapp, Facebook, Instagram, E-mail e SMS."
+          dataWork="Setembro 2023"
         />
       </div>
     </div>
